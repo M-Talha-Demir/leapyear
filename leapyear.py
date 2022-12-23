7c43ed1af965c34c080723891e96e35931aa323b
Muhammed Talha Demir <muhammedtalha.demir@hotmail.com>
2022-12-23 09:38:23 +0300

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,13 @@
+def leapYear(year):
+    if year % 4 == 0:
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+
+
+leapYear(2000)
+leapYear(1000)
+leapYear(2001)
+leapYear(2004)
+leapYear(2005)
+leapYear(2008)
